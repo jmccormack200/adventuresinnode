a0c8365c9f4be553fcde4b09d651dbfeb557922b
John McCormack <mccormack.wgsi@gmail.com>
2015-07-18 14:12:44 -0400

    Finished Chapter 15

---
+++ Section3/Chapter15/doc_aggregate.js
@@ -0,0 +1,28 @@
+var MongoClient = require('mongodb').MongoClient;
+MongoClient.connect("mongodb://localhost/", function(err, db){
+	var myDB = db.db("words");
+	myDB.collection("word_stats", aggregateItems);
+	setTimeout(function(){
+		db.close();
+	}, 3000);
+});
+function aggregateItems(err, words){
+	words.aggregate([{$match: {first:{$in:['a', 'e', 'i', 'o', 'u']}}},
+			{$group: {_id:"$first",
+				largest:{$max:"$size"},
+				smallest:{$min:"$size"},
+				total:{$sum:1}}},
+			{$sort: {_id:1}}],
+			function(err, results){
+				console.log("Largest and smallest word sizes for " +
+						"words beginning with a vowel: ");
+				console.log(results);
+			});
+		words.aggregate([{$match: {size:4}},
+					{$limit: 5},
+					{$project: {_id:"$word", stats:1}}],
+				function(err, results){
+					console.log("Stats for 5 four letter words: ");
+					console.log(results);
+				});
+}
